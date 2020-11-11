4be4d3bcdcfcff9490499cad0c076abdd6219f78
Nick Braica <nick.braica@gmail.com>
2020-11-11 12:43:27 -0800

    Initial scaffold

---
+++ actblue.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     ActBlue
+ * Plugin URI:      https://secure.actblue.com/
+ * Description:     Enhance the ActBlue experience.
+ * Author:          ActBlue
+ * Author URI:      https://secure.actblue.com/
+ * Text Domain:     actblue
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         ActBlue
+ */
+
+// Your code starts here.
