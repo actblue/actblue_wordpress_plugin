b7f9c0f9b84cfca83555c86bb64e68520005bad7
Nick Braica <nick.braica@gmail.com>
2020-11-19 10:06:14 -0800

    Update spaces

--- actblue/tests/test-settings-staging.php
+++ actblue/tests/test-settings-staging.php
@@ -9,21 +9,21 @@
  * Test case for the settings.
  */
 class ActBlueStagingTest extends WP_UnitTestCase {
-	// These are required to tell PHPUnit _not_ to preserve global state for this
-	// test since we're setting ups constants.
-	protected $preserveGlobalState = FALSE;
+    // These are required to tell PHPUnit _not_ to preserve global state for this
+    // test since we're setting ups constants.
+    protected $preserveGlobalState = FALSE;
     protected $runTestInSeparateProcess = TRUE;
 
-	/**
-	 * Test the actblue_get_url() function in development mode.
-	 */
-	public function test_host_url_staging() {
-		define( 'ACTBLUE_ENV', 'development' );
-		define( 'ACTBLUE_STAGING_HOST', 'https://staging.actblue.com' );
+    /**
+     * Test the actblue_get_url() function in development mode.
+     */
+    public function test_host_url_staging() {
+        define( 'ACTBLUE_ENV', 'development' );
+        define( 'ACTBLUE_STAGING_HOST', 'https://staging.actblue.com' );
 
-		$url = actblue_get_url( '/cf/assets/actblue.js' );
-		$expected = 'https://staging.actblue.com/cf/assets/actblue.js';
+        $url = actblue_get_url( '/cf/assets/actblue.js' );
+        $expected = 'https://staging.actblue.com/cf/assets/actblue.js';
 
-		$this->assertEquals( $expected, $url );
-	}
+        $this->assertEquals( $expected, $url );
+    }
 }
