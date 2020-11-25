1d8cd33cf434c434cd424eecfc15136602a94992
Nick Braica <nick.braica@gmail.com>
2020-11-25 09:44:33 -0800

    Add capability to run the script locally

--- .circleci/deploy-assets.sh
+++ .circleci/deploy-assets.sh
@@ -1,11 +1,37 @@
 #!/usr/bin/env bash
 
-if [[ -z "$CIRCLECI" ]]; then
-    echo "This script can only be run by CircleCI. Aborting." 1>&2
-    exit 1
-fi
+while getopts "u:p:h" opt; do
+    case ${opt} in
+        u )
+            WP_ORG_USERNAME=$OPTARG
+            ;;
+        p )
+            WP_ORG_PASSWORD=$OPTARG
+            ;;
+        h )
+            echo "Usage: deploy-plugin.sh [-u <username>] [-p <password>]"
+            echo ""
+            echo "Deploys the plugin to the WordPress svn repository. If you run this"
+            echo "locally, you'll either need to pass the WordPress.org username and"
+            echo "password when running this script or have the \$WP_ORG_PASSWORD and"
+            echo "\$WP_ORG_USERNAME environment variables set."
+            echo ""
+            echo "  -u  Set the WordPress.org username of the authorized user."
+            echo "  -p  Set the WordPress.org password for the authorized user."
+            exit 0
+            ;;
+        \? )
+            echo "Invalid option: $OPTARG" 1>&2
+            ;;
+        : )
+            echo "Invalid option: $OPTARG requires an argument" 1>&2
+            ;;
+    esac
+done
+shift $((OPTIND -1))
 
-if [[ -z "$CIRCLE_BRANCH" || "$CIRCLE_BRANCH" != "main" ]]; then
+# If this is being run by CircleCI, make sure we're on the `main` branch.
+if [[ -n "$CIRCLECI" && ( -z "$CIRCLE_BRANCH" || "$CIRCLE_BRANCH" != "main" ) ]]; then
     echo "Build branch is required and must be 'main' branch. Stopping deployment." 1>&2
     exit 0
 fi
@@ -15,17 +41,13 @@ if [[ -z "$WP_ORG_PASSWORD" ]]; then
     exit 1
 fi
 
-if [[ -z "$WP_ORG_PLUGIN_NAME" ]]; then
-    echo "WordPress.org plugin name not set. Aborting." 1>&2
-    exit 1
-fi
-
 if [[ -z "$WP_ORG_USERNAME" ]]; then
     echo "WordPress.org username not set. Aborting." 1>&2
     exit 1
 fi
 
-SVN_URL=${SVN_URL:-"https://plugins.svn.wordpress.org/${WP_ORG_PLUGIN_NAME}/"}
+WP_ORG_PLUGIN_NAME="actblue-contributions"
+SVN_URL=${SVN_URL:-"https://plugins.svn.wordpress.org/${WP_ORG_PLUGIN_NAME}"}
 PLUGIN_SVN_PATH="/tmp/svn"
 
 # Checkout the SVN repo
--- .circleci/deploy-plugin.sh
+++ .circleci/deploy-plugin.sh
@@ -1,11 +1,38 @@
 #!/usr/bin/env bash
 
-if [[ -z "$CIRCLECI" ]]; then
-    echo "This script can only be run by CircleCI. Aborting." 1>&2
-    exit 1
-fi
+while getopts "u:p:h" opt; do
+    case ${opt} in
+        u )
+            WP_ORG_USERNAME=$OPTARG
+            ;;
+        p )
+            WP_ORG_PASSWORD=$OPTARG
+            ;;
+        h )
+            echo "Usage: deploy-plugin.sh [-u <username>] [-p <password>]"
+            echo ""
+            echo "Deploys the plugin to the WordPress svn repository. If you run this"
+            echo "locally, you'll either need to pass the WordPress.org username and"
+            echo "password when running this script or have the \$WP_ORG_PASSWORD and"
+            echo "\$WP_ORG_USERNAME environment variables set."
+            echo ""
+            echo "  -u  Set the WordPress.org username of the authorized user."
+            echo "  -p  Set the WordPress.org password for the authorized user."
+            exit 0
+            ;;
+        \? )
+            echo "Invalid option: $OPTARG" 1>&2
+            ;;
+        : )
+            echo "Invalid option: $OPTARG requires an argument" 1>&2
+            ;;
+    esac
+done
+shift $((OPTIND -1))
 
-if [[ -z "$CIRCLE_BRANCH" || "$CIRCLE_BRANCH" != "main" ]]; then
+
+# If this is being run by CircleCI, make sure we're on the `main` branch.
+if [[ -n "$CIRCLECI" && ( -z "$CIRCLE_BRANCH" || "$CIRCLE_BRANCH" != "main" ) ]]; then
     echo "Build branch is required and must be 'main' branch. Stopping deployment." 1>&2
     exit 0
 fi
@@ -15,17 +42,13 @@ if [[ -z "$WP_ORG_PASSWORD" ]]; then
     exit 1
 fi
 
-if [[ -z "$WP_ORG_PLUGIN_NAME" ]]; then
-    echo "WordPress.org plugin name not set. Aborting." 1>&2
-    exit 1
-fi
-
 if [[ -z "$WP_ORG_USERNAME" ]]; then
     echo "WordPress.org username not set. Aborting." 1>&2
     exit 1
 fi
 
-SVN_URL=${SVN_URL:-"https://plugins.svn.wordpress.org/${WP_ORG_PLUGIN_NAME}/"}
+WP_ORG_PLUGIN_NAME="actblue-contributions"
+SVN_URL=${SVN_URL:-"https://plugins.svn.wordpress.org/${WP_ORG_PLUGIN_NAME}"}
 PLUGIN_BUILD_PATH="/tmp/build"
 PLUGIN_SVN_PATH="/tmp/svn"
 
@@ -36,7 +59,7 @@ LATEST_GIT_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
 LATEST_SVN_TAG=${LATEST_GIT_TAG:1}
 
 # Check if the latest SVN tag exists already
-TAG=$(svn ls "${SVN_URL}tags/${LATEST_SVN_TAG}")
+TAG=$(svn ls "${SVN_URL}/tags/${LATEST_SVN_TAG}")
 error=$?
 if [ $error == 0 ]; then
     # Tag exists, don't deploy
