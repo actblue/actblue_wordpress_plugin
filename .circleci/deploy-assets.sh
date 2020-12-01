e12d43b3832d363162cf10a8ab8a2a6629d01811
Nick Braica <nick.braica@gmail.com>
2020-12-01 11:31:45 -0800

    Set up deployment scripts for CircleCI (#26)
    * Update gitignore
    
    * Add the assets directory
    
    * Scaffold
    
    * Align deployment scripts
    
    * Update asset location
    
    * We need svn for the unit tests
    
    * Update local svn repo for testing
    
    * Swap back to prd url
    
    * Update local repo location
    
    * Need to update repository
    
    * Improve usability for testing
    
    * Add capability to run the script locally
    
    * Docs around deployment
    
    * Update README.md
    
    Co-authored-by: Mike Burns <mike.burns@upstatement.com>
    
    * Reset state for local deployments
    
    Co-authored-by: Mike Burns <mike.burns@upstatement.com>

---
+++ .circleci/deploy-assets.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+while getopts "u:p:h" opt; do
+    case ${opt} in
+        u )
+            WP_ORG_USERNAME=$OPTARG
+            ;;
+        p )
+            WP_ORG_PASSWORD=$OPTARG
+            ;;
+        h )
+            echo "Usage: deploy-plugin.sh [-u <username>] [-p <password>]"
+            echo ""
+            echo "Deploys the plugin to the WordPress svn repository. If you run this"
+            echo "locally, you'll either need to pass the WordPress.org username and"
+            echo "password when running this script or have the \$WP_ORG_PASSWORD and"
+            echo "\$WP_ORG_USERNAME environment variables set."
+            echo ""
+            echo "  -u  Set the WordPress.org username of the authorized user."
+            echo "  -p  Set the WordPress.org password for the authorized user."
+            exit 0
+            ;;
+        \? )
+            echo "Invalid option: $OPTARG" 1>&2
+            ;;
+        : )
+            echo "Invalid option: $OPTARG requires an argument" 1>&2
+            ;;
+    esac
+done
+shift $((OPTIND -1))
+
+# If this is being run by CircleCI, make sure we're on the `main` branch.
+if [[ -n "$CIRCLECI" && ( -z "$CIRCLE_BRANCH" || "$CIRCLE_BRANCH" != "main" ) ]]; then
+    echo "Build branch is required and must be 'main' branch. Stopping deployment." 1>&2
+    exit 0
+fi
+
+if [[ -z "$WP_ORG_PASSWORD" ]]; then
+    echo "WordPress.org password not set. Aborting." 1>&2
+    exit 1
+fi
+
+if [[ -z "$WP_ORG_USERNAME" ]]; then
+    echo "WordPress.org username not set. Aborting." 1>&2
+    exit 1
+fi
+
+WP_ORG_PLUGIN_NAME="actblue-contributions"
+SVN_URL=${SVN_URL:-"https://plugins.svn.wordpress.org/${WP_ORG_PLUGIN_NAME}"}
+PLUGIN_SVN_PATH="/tmp/svn"
+
+# Checkout the SVN repo
+svn checkout -q $SVN_URL $PLUGIN_SVN_PATH
+
+# Delete the assets directory
+rm -rf $PLUGIN_SVN_PATH/assets
+
+# Copy our plugin assets as the new assets directory
+rsync -rc --exclude=".gitkeep" ./assets/ $PLUGIN_SVN_PATH/assets/
+
+# Move to SVN directory
+cd $PLUGIN_SVN_PATH
+
+# Add new files to SVN
+svn stat | grep '^?' | awk '{print $2}' | xargs -I x svn add x@
+
+# Remove deleted files from SVN
+svn stat | grep '^!' | awk '{print $2}' | xargs -I x svn rm --force x@
+
+# Commit to SVN
+svn commit --no-auth-cache --username $WP_ORG_USERNAME --password $WP_ORG_PASSWORD -m "Deploy new assets"
