3c3ad644e4558261e755543f3fed842e7ed65e6e
Nick Braica <nick.braica@gmail.com>
2020-12-02 11:00:27 -0800

    Cleanup

--- actblue-contributions/blocks/index.js
+++ actblue-contributions/blocks/index.js
@@ -1,22 +1,13 @@
 /**
  * WordPress dependencies
  */
-// import domReady from "@wordpress/dom-ready";
 import { registerBlockType } from "@wordpress/blocks";
 
 /**
  * Internal dependencies
  */
-// import * as embedBlock from "./custom/actblue-embed";
 import * as buttonsBlock from "./custom/actblue-buttons";
 import * as buttonBlock from "./custom/actblue-button";
-
-// We need to run this on domReady so that the block data has been initialized.
-// domReady(() => {
-// The `settings` parameter needs to be a function so we can get the
-// `core/embed` settings to extend for this block.
-// registerBlockType(embedBlock.name, embedBlock.getSettings());
-// });
 
 [buttonsBlock, buttonBlock].forEach(({ name, settings }) => {
 	registerBlockType(name, settings);
