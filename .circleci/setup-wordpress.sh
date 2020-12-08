3883956df6c7d8f866d836c86a0bac5d2b10deda
Nick Braica <nick.braica@gmail.com>
2020-12-07 17:47:08 -0800

    More loc tests

--- .circleci/setup-wordpress.sh
+++ .circleci/setup-wordpress.sh
@@ -1,32 +1,34 @@
 #!/usr/bin/env bash
 
-if [[ -z "$CIRCLECI" ]]; then
-    echo "This script can only be run by CircleCI. Aborting." 1>&2
-    exit 1
-fi
+ls -la
 
-if [[ -z "$WP_CORE_DIR" ]]; then
-    echo "WordPress core directory isn't set. Aborting." 1>&2
-    exit 1
-fi
+# if [[ -z "$CIRCLECI" ]]; then
+#     echo "This script can only be run by CircleCI. Aborting." 1>&2
+#     exit 1
+# fi
 
-if [[ -z "$WP_HOST" ]]; then
-    echo "WordPress host isn't set. Aborting." 1>&2
-    exit 1
-fi
+# if [[ -z "$WP_CORE_DIR" ]]; then
+#     echo "WordPress core directory isn't set. Aborting." 1>&2
+#     exit 1
+# fi
 
-if [[ -z "$WP_PLUGIN_NAME" ]]; then
-    echo "WordPress.org plugin name not set. Aborting." 1>&2
-    exit 1
-fi
+# if [[ -z "$WP_HOST" ]]; then
+#     echo "WordPress host isn't set. Aborting." 1>&2
+#     exit 1
+# fi
 
-# Install WordPress
-actblue-contributions/vendor/bin/wp config create --path="${WP_CORE_DIR}" --dbhost="127.0.0.1" --dbname="circle_test" --dbuser="root"
-actblue-contributions/vendor/bin/wp core install --path="${WP_CORE_DIR}" --url="http://${WP_HOST}" --title="Passwords Evolved Test" --admin_user="admin" --admin_password="password" --admin_email="admin@example.com"
-actblue-contributions/vendor/bin/wp rewrite structure --path="${WP_CORE_DIR}" '/%postname%/'
+# if [[ -z "$WP_PLUGIN_NAME" ]]; then
+#     echo "WordPress.org plugin name not set. Aborting." 1>&2
+#     exit 1
+# fi
 
-# Copy our plugin to WordPress directory
-cp -r ./actblue-contributions ${WP_CORE_DIR}/wp-content/plugins/${WP_PLUGIN_NAME}
+# # Install WordPress
+# actblue-contributions/vendor/bin/wp config create --path="${WP_CORE_DIR}" --dbhost="127.0.0.1" --dbname="circle_test" --dbuser="root"
+# actblue-contributions/vendor/bin/wp core install --path="${WP_CORE_DIR}" --url="http://${WP_HOST}" --title="Passwords Evolved Test" --admin_user="admin" --admin_password="password" --admin_email="admin@example.com"
+# actblue-contributions/vendor/bin/wp rewrite structure --path="${WP_CORE_DIR}" '/%postname%/'
 
-# Activate our plugin
-actblue-contributions/vendor/bin/wp plugin activate --path="${WP_CORE_DIR}" ${WP_PLUGIN_NAME}
+# # Copy our plugin to WordPress directory
+# cp -r ./actblue-contributions ${WP_CORE_DIR}/wp-content/plugins/${WP_PLUGIN_NAME}
+
+# # Activate our plugin
+# actblue-contributions/vendor/bin/wp plugin activate --path="${WP_CORE_DIR}" ${WP_PLUGIN_NAME}
