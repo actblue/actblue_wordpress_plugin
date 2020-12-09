f8667e0dd253e25a1add1cb8b0e33d41247cfe84
Nick Braica <nick.braica@gmail.com>
2020-12-09 10:47:54 -0800

    Reset database

--- actblue-contributions/bin/install-wp-e2e-tests.sh
+++ actblue-contributions/bin/install-wp-e2e-tests.sh
@@ -8,16 +8,22 @@ until mysqlshow -uwordpress -pwordpress -hdb wordpress; do
   sleep 1
 done
 
-# Configure WordPress
-if [ ! -f wp-config.php ]; then
-	echo "Configuring WordPress..."
-	wp config create \
-		--allow-root \
-		--dbname=$WORDPRESS_DB_NAME \
-		--dbuser=$WORDPRESS_DB_USER \
-		--dbpass=$WORDPRESS_DB_PASSWORD \
-		--dbhost=$WORDPRESS_DB_HOST
+# Drop the database if it exists.
+mysql -u $WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD -h $WORDPRESS_DB_HOST -e "drop database $WORDPRESS_DB_NAME"
+mysql -u $WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD -h $WORDPRESS_DB_HOST -e "create database $WORDPRESS_DB_NAME"
+
+if [ -f wp-config.php ]; then
+	rm wp-config.php
 fi
+
+# Configure WordPress
+echo "Configuring WordPress..."
+wp config create \
+	--allow-root \
+	--dbname=$WORDPRESS_DB_NAME \
+	--dbuser=$WORDPRESS_DB_USER \
+	--dbpass=$WORDPRESS_DB_PASSWORD \
+	--dbhost=$WORDPRESS_DB_HOST
 
 # Run WP install if it's not already installed.
 if ! $(wp core is-installed); then
