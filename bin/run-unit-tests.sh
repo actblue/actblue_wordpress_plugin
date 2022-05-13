1e671a4e2d552d4743011866f7a78b6182312530
Antoinette Smith <asmith@actbluetech.com>
2022-05-13 15:39:26 -0500

    modify local unit test runner to install dev dependencies and run tests via vendor version of phpunit

--- bin/run-unit-tests.sh
+++ bin/run-unit-tests.sh
@@ -2,6 +2,8 @@
 
 docker-compose -f docker-compose.yml up -d --build
 
+docker-compose exec wordpress composer install
+
 until $(curl --output /dev/null --silent --head --fail http://localhost); do
     printf '.'
     sleep 5
@@ -9,5 +11,5 @@ done
 
 echo
 
-docker-compose exec wordpress phpunit
+docker-compose exec wordpress vendor/bin/phpunit
 docker-compose stop
