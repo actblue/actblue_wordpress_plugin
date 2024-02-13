a64c55725d4e5700374c073df50f7fd45c621f68
Maxine Krebs <mkrebs@actbluetech.com>
2024-02-13 13:41:09 -0500

    Increment version number

--- actblue-contributions/actblue-contributions.php
+++ actblue-contributions/actblue-contributions.php
@@ -14,7 +14,7 @@
  * License URI:      http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:      actblue
  * Domain Path:      /languages
- * Version:          1.5.3
+ * Version:          1.5.4
  */
 
 // If this file is called directly, abort.
@@ -25,7 +25,7 @@ if ( ! defined( 'WPINC' ) ) {
 /**
  * Current plugin version.
  */
-define( 'ACTBLUE_PLUGIN_VERSION', '1.5.3' );
+define( 'ACTBLUE_PLUGIN_VERSION', '1.5.4' );
 
 /**
  * Defines the ActBlue host.
